3c22857e8eb9180b81eecb0593d4f33cb018dd84
Lihopps <lihops.game@gmail.com>
2026-07-03 21:15:36 +0200

    UPGRAE FOR 2.1

--- script/main.lua
+++ script/main.lua
@@ -11,6 +11,10 @@ end
 function main.on_configuration_changed(e)
     if not storage.lihop_buildings then storage.lihop_buildings = {} end
 	
+    local old_version = e.mod_changes[script.mod_name] and e.mod_changes[script.mod_name].old_version
+    if not old_version then
+        return
+    end
     for version, migration in pairs(migrations) do
         if helpers.compare_versions(version, old_version) > 0 then
             migration()
